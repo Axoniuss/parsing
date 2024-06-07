bf69b870eaec86860824e455a394c33c6597e614
Axoniuss <klim.vlad@list.ru>
2024-06-07 12:49:57 +0300

    api index

--- class.php
+++ class.php
@@ -1,4 +1,35 @@
 <?php
-   $arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
-   echo json_encode($arr);
-?>
+$json = '{"1:"Значение 1"}';
+$array = json_decode($json, true);
+ 
+switch (json_last_error()) {
+	case JSON_ERROR_NONE:
+		echo 'Ошибок нет';
+	break;
+	case JSON_ERROR_DEPTH:
+		echo 'Достигнута максимальная глубина стека';
+	break;
+	case JSON_ERROR_STATE_MISMATCH:
+		echo 'Некорректные разряды или несоответствие режимов';
+	break;
+	case JSON_ERROR_CTRL_CHAR:
+		echo 'Некорректный управляющий символ';
+	break;
+	case JSON_ERROR_SYNTAX:
+		echo 'Синтаксическая ошибка, некорректный JSON';
+	break;
+	case JSON_ERROR_UTF8:
+		echo 'Некорректные символы UTF-8, возможно неверно закодирован';
+	break;
+	default:
+		echo 'Неизвестная ошибка';
+	break;
+}
+$constants = get_defined_constants(true);
+
+foreach ($constants['json'] as $name => $value) {
+
+	echo $name . ': ' . $value . '<br>';
+
+}
+   ?>
